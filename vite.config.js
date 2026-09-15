43e53d1a9a3333c9e0f1bb124a3587a951424c20
Achim Imboden <imbodi55@hotmail.com>
2026-09-15 09:25:20 +0200

    Configure multi-page build

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        useCase: resolve(__dirname, 'use-case/index.html'),
+        openSource: resolve(__dirname, 'materials/open-source/index.html'),
+        governance: resolve(__dirname, 'materials/m365-governance/index.html')
+      }
+    }
+  }
+});
